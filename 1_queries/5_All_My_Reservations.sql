16d632a4c574f61f46b168a3384ae082341ba03e
Kevin Tan Yulo <yulokevin@gmail.com>
2022-01-05 14:24:17 -0800

    refactor

--- 1_queries/5_All_My_Reservations.sql
+++ 1_queries/5_All_My_Reservations.sql
@@ -4,8 +4,18 @@ JOIN reservations ON reservations.property_id = properties.id
 JOIN property_reviews ON property_reviews.property_id = properties.id
 JOIN users ON users.id = properties.owner_id
 WHERE reservations.guest_id = 1
-GROUP BY properties.id, properties.title, cost_per_night, reservations.start_date, reservations.end_date 
-HAVING reservations.end_date < now()::date
+AND reservations.end_date < now()::date
+GROUP BY properties.id, properties.title, cost_per_night, reservations.start_date, reservations.end_date
 ORDER BY reservations.start_date
 LIMIT 10
 ;
+
+-- SELECT properties.*, reservations.*, avg(rating) as average_rating
+-- FROM reservations
+-- JOIN properties ON reservations.property_id = properties.id
+-- JOIN property_reviews ON properties.id = property_reviews.property_id
+-- WHERE reservations.guest_id = 1
+-- AND reservations.end_date < now()::date
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date
+-- LIMIT 10;
